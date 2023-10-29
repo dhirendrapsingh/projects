317529be9b04a452573bcb3d0c83df3b391abac0
dhirendrapsingh <dhirendrapsingh@users.noreply.github.com>
2023-10-29 17:27:30 -0500

    Add files via upload

---
+++ MyNotesApp/src/test/java/com/dp/projects/MyNotesApp/MyNotesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.projects.MyNotesApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNotesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
